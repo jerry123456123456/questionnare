8609a23a5ba45fb36adc6e5b3d091af6d75daffc
jerry123456123456 <songqy2004@qq.com>
2024-11-11 07:46:48 +0800

    2024-11-12

--- questionnare-src/api/api_unreg.cc
+++ questionnare-src/api/api_unreg.cc
@@ -61,7 +61,7 @@ std::string formatString2(const std::string &format, Args... args) {
                        buf.get() + size - 1); // We don't want the '\0' inside
 }
 
-int unregisterUser(string &user_name,string &token){
+int unregisterUser(string &user_name, string &token) {
     int ret = 0;
     uint32_t user_id;
     CDBManager *db_manager = CDBManager::getInstance();
@@ -70,22 +70,79 @@ int unregisterUser(string &user_name,string &token){
 
     string str_sql;
     // 查询用户是否已存在的SQL语句
-    printf("%s\n",user_name.c_str());
+    printf("%s\n", user_name.c_str());
     str_sql = formatString2("select user_id from Users where user_name='%s'", user_name.c_str());
     CResultSet *result_set = db_conn->ExecuteQuery(str_sql.c_str());
 
-    if(result_set && result_set->Next()){  //存在，准备删除
-        // 如果用户不存在，则进行注册
-        int user_id = result_set->GetInt("user_id");
-        str_sql = "delete from Users where user_id = ?";
-        LogInfo("执行: {}", str_sql);
+    if (result_set && result_set->Next()) {  // 存在，准备删除
+        // 获取用户ID
+        user_id = result_set->GetInt("user_id");
 
-        // 必须在释放连接前delete
-        // CPrepareStatement对象，否则有可能多个线程操作mysql对象，会crash
+        // 先删除Responses表中与该用户相关的记录（通过关联Surveys和Questions表）
+        str_sql = "delete from Responses where user_id =? or survey_id in (select survey_id from Surveys where user_id =?) or question_id in (select question_id from Questions where survey_id in (select survey_id from Surveys where user_id =?))";
+        LogInfo("执行: {}", str_sql);
+        CPrepareStatement *stmt_responses = new CPrepareStatement();
+        if (stmt_responses->Init(db_conn->GetMysql(), str_sql)) {
+            stmt_responses->SetParam(0, user_id);
+            stmt_responses->SetParam(1, user_id);
+            stmt_responses->SetParam(2, user_id);
+            bool bRet_responses = stmt_responses->ExecuteUpdate();
+            if (!bRet_responses) {
+                LogError("delete Responses related to user failed. {}", str_sql);
+                ret = 1;
+            }
+        }
+        delete stmt_responses;
+
+        // 删除Options表中与该用户相关的记录（通过关联Questions表）
+        str_sql = "delete from Options where question_id in (select question_id from Questions where survey_id in (select survey_id from Surveys where user_id =?))";
+        LogInfo("执行: {}", str_sql);
+        CPrepareStatement *stmt_options = new CPrepareStatement();
+        if (stmt_options->Init(db_conn->GetMysql(), str_sql)) {
+            stmt_options->SetParam(0, user_id);
+            bool bRet_options = stmt_options->ExecuteUpdate();
+            if (!bRet_options) {
+                LogError("delete Options related to user failed. {}", str_sql);
+                ret = 1;
+            }
+        }
+        delete stmt_options;
+
+        // 删除Questions表中与该用户相关的记录（通过关联Surveys表）
+        str_sql = "delete from Questions where survey_id in (select survey_id from Surveys where user_id =?)";
+        LogInfo("执行: {}", str_sql);
+        CPrepareStatement *stmt_questions = new CPrepareStatement();
+        if (stmt_questions->Init(db_conn->GetMysql(), str_sql)) {
+            stmt_questions->SetParam(0, user_id);
+            bool bRet_questions = stmt_questions->ExecuteUpdate();
+            if (!bRet_questions) {
+                LogError("delete Questions related to user failed. {}", str_sql);
+                ret = 1;
+            }
+        }
+        delete stmt_questions;
+
+        // 删除Surveys表中该用户的相关记录
+        str_sql = "delete from Surveys where user_id =?";
+        LogInfo("执行: {}", str_sql);
+        CPrepareStatement *stmt_surveys = new CPrepareStatement();
+        if (stmt_surveys->Init(db_conn->GetMysql(), str_sql)) {
+            stmt_surveys->SetParam(0, user_id);
+            bool bRet_surveys = stmt_surveys->ExecuteUpdate();
+            if (!bRet_surveys) {
+                LogError("delete Surveys related to user failed. {}", str_sql);
+                ret = 1;
+            }
+        }
+        delete stmt_surveys;
+
+        // 最后删除Users表中的用户记录
+        str_sql = "delete from Users where user_id =?";
+        LogInfo("执行: {}", str_sql);
         CPrepareStatement *stmt = new CPrepareStatement();
-        if(stmt->Init(db_conn->GetMysql(),str_sql)){
+        if (stmt->Init(db_conn->GetMysql(), str_sql)) {
             uint32_t index = 0;
-            stmt->SetParam(index++,user_id);
+            stmt->SetParam(index++, user_id);
             bool bRet = stmt->ExecuteUpdate();
             if (bRet) {
                 ret = 0;
@@ -95,10 +152,12 @@ int unregisterUser(string &user_name,string &token){
             }
         }
         delete stmt;
-    }else{  //不存在
+    } else {  // 不存在
         delete result_set;
         ret = 2;
     }
+
+    // 接下来删除
     return ret;
 }
 
@@ -128,7 +187,7 @@ int ApiunRegisterUser(string &url, string &post_data, string &str_json){
         goto END;
     }
 
-    //注册账号
+    //注销账号
     ret = unregisterUser(user_name,pwd);
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!标签只是一个位置，正常顺序执行过来的代码也会执行下面的逻辑
